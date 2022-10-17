c18900171d5c474962412e7ec7265b5a3d80a9a2
vacrain <726866011@qq.com>
2022-10-17 08:59:27 +0800

    build path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  base: 'production' === process.env.NODE_ENV ? '/你要部署的仓库名称/' : `./`,
 })
