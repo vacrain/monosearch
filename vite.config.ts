f29f1a51344b792dce0d5fc124e920534ae786ed
vacrain <726866011@qq.com>
2022-10-17 09:02:11 +0800

    修改 build path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'production' === process.env.NODE_ENV ? '/你要部署的仓库名称/' : `./`,
+  base: 'production' === process.env.NODE_ENV ? '/monosearch/' : `./`,
 })
